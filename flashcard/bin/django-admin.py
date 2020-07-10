2cace17d51692f889d5429682f57dd2da92202eb
ishimoto <ishimoto@umich.edu>
2020-07-10 14:29:13 -0400

    My Django Girls app, first commit

---
+++ flashcard/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ishimoto/Desktop/Ross/Business/App Dev/practice/djangogirls/flashcard/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
